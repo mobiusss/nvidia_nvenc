de7519cb3848e3df4c3208ca1e974e3aa94e89a6
mobiusss <mobius@mobiusmobiusmobius.com>
2023-03-04 03:13:48 -0500

    fixing install

--- install.sh
+++ install.sh
@@ -1,26 +1,29 @@
-# Install dependencies (replace XXX in libnvidia-encode-XXX, libnvidia-decode-XXX with your driver version)
-# libnvidia-encode is part of the driver meta package so if your driver works fine you might already have it ! 
-apt install -y \
-          libavfilter-dev \
-          libavformat-dev \
-          libavcodec-dev \
-          libswresample-dev \
-          libavutil-dev\
-          wget \
-          cmake \
-          build-essential \
-          git \
-          libnvidia-encode-525 \
-          libnvidia-decode-525 
-# Install CUDA Toolkit (if not already present)
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
+# Install dependencies
+# replace NV_DRIVER_VER with your driver version
+NV_DRIVER_VER=525
+KEY_PKG=cuda-keyring_1.0-1_all.deb
+
+# Install CUDA Toolkit
+KEY_URL=https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/$KEY_PKG
+curl -X GET $KEY_URL > $KEY_PKG
+sudo dpkg -i $KEY_PKG
 sudo apt-get update
-sudo apt-get install -y cuda
-# Ensure nvcc to your $PATH (most commonly already done by the CUDA installation)
+
+sudo apt install -y --no-install-recommends \
+     python3-dev libavfilter-dev libavformat-dev \
+     libavcodec-dev libswresample-dev libavutil-dev \
+     wget cmake build-essential git cuda \
+     libnvidia-encode-$NV_DRIVER_VER \
+     libnvidia-decode-$NV_DRIVER_VER
+
+# make a safe venv
+python3 -m venv venv
+
+# Ensure nvcc on $PATH (commonly already done by CUDA install)
 export PATH=/usr/local/cuda/bin:$PATH
 
-# Install VPF
-pip3 install git+https://github.com/NVIDIA/VideoProcessingFramework
-# or if you cloned this repository
-pip3 install .
+# Install VideoProcessingFramework
+git clone https://github.com/NVIDIA/VideoProcessingFramework
+venv/bin/pip install torch torchvision tqdm
+venv/bin/pip install VideoProcessingFramework/. VideoProcessingFramework/src/PytorchNvCodec/.
+	     
